f84a48978073c70474b2e73d9d509e2a00331d97
Thanh Minh & Hoan <90053384+thanhminh1510@users.noreply.github.com>
2024-03-25 11:48:59 +0700

    clean code

--- quizz-app-client/src/App.jsx
+++ quizz-app-client/src/App.jsx
@@ -1,35 +1,31 @@
-
-import './App.css'
+import './App.css';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route } from 'react-router-dom';
 import Login from './pages/LoginPage/Login';
 import Logup from './pages/LogupPage/Logup';
 import Home from './pages/HomePage/Home';
-function App() {
 
+function App() {
   const darkTheme = createTheme({
     palette: {
       mode: 'dark',
     },
   });
-  
 
   return (
-
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <main>
         <Routes>
-
-          <Route path="/login" element={ <Login/> } />
-          <Route path="/logup" element={ <Logup/> } />
-
+          {/* Set the default page as the homepage */}
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logup" element={<Logup />} />
         </Routes>
-      <Home />
       </main>
-  </ThemeProvider>
-  )
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
