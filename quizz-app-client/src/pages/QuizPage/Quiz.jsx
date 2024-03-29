ae8a5907e23fbae3c0b3bf840f43722703c007e4
Thanh Minh & Hoan <90053384+thanhminh1510@users.noreply.github.com>
2024-03-29 17:26:04 +0700

    add css page for quiz page

--- quizz-app-client/src/pages/QuizPage/Quiz.jsx
+++ quizz-app-client/src/pages/QuizPage/Quiz.jsx
@@ -3,13 +3,19 @@ import test from "../../assets/test.jpg"
 export default function Quiz() {
   return (
   <div className="container">
-    <div className="d-flex flex-column quiz-content w-full row">
-      <section className="question-wrapper col-lg-6 col-md-12 col-sm-12">
-          <div className="question-box">
-            <div className="question-box__img">
-              <img src={test} alt="Logo" />
+    <div className="d-flex flex-column quiz-content w-full">
+      <section className="question-wrapper">
+          <div className="question-box row">
+              <div className="bg-white col-lg-6 col-md-12 col-sm-12 rounded">
+                <div className="question-box__img ">
+                  <img src={test} alt="Logo" width="100%"/>
+                </div>
             </div>
-            <div className="question-box__text"></div>
+            <div className="question-box__text col-lg-6 col-md-12 col-sm-12">
+               <p>
+                Các quốc gia có mức độ hạnh phúc cao nhất trên thế giới. Trò chơi Angry Birds đến từ đất nước này. Vào mùa hè, đất nước này có 19 tiếng ban ngày. Vào mùa đông, có 19 tiếng ban đêm.
+               </p>
+            </div>
 
           </div>
       </section>
